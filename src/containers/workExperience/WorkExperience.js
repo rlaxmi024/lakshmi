4df7202baf246485f0f3e4732a1bbc5b4aac17f0
Lakshmi Ramesh <rlaxmi024@gmail.com>
2023-04-29 00:38:25 -0400

    Update WorkExperience.js

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -2,7 +2,6 @@ import React, {useContext} from "react";
 import "./WorkExperience.scss";
 import ExperienceCard from "../../components/experienceCard/ExperienceCard";
 import {workExperiences} from "../../portfolio";
-import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function WorkExperience() {
