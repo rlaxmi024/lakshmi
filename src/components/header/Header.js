1965e7947af94e7549cd1b85cfe808f40e483c80
Lakshmi Ramesh <rlaxmi024@gmail.com>
2023-05-01 12:47:36 -0400

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -26,9 +26,7 @@ function Header() {
     <Headroom>
       <header className={isDark ? "dark-menu header" : "header"}>
         <a href="/" className="logo">
-          <span className="grey-color"> &lt;</span>
           <span className="logo-name">{greeting.username}</span>
-          <span className="grey-color">/&gt;</span>
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label
